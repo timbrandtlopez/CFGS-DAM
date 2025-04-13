285495f298dc4ff2b936019871dc02fbb59ec04c
Tim Brandt <timbrandtlopez@gmail.com>
2025-04-13 13:51:38 +0200

    Programación - 05/03/2025

---
+++ PROGRAMACIÓN/sesion_2025_03_05/Constantes.java
@@ -0,0 +1,15 @@
+package sesion_2025_03_05;
+
+public class Constantes {
+
+    public static void main(String[] args) {
+
+        final double PI = 3.1416; // constante de tipo double
+        final int EDAD_MAXIMA = 100; // constante de tipo entero
+
+        System.out.println("El valor de PI es: " + PI);
+        System.out.println("La edad máxima permitida es: " + EDAD_MAXIMA);
+
+    }
+
+}
---
+++ PROGRAMACIÓN/sesion_2025_03_05/OperadoresAritmeticos.java
@@ -0,0 +1,16 @@
+package sesion_2025_03_05;
+
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+
+        int a = 10, b = 5;
+
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Resta: " + (a - b));
+        System.out.println("Multiplicación: " + (a * b));
+        System.out.println("División: " + (a / b));
+        System.out.println("Módulo: " + (a % b));
+    }
+
+}
