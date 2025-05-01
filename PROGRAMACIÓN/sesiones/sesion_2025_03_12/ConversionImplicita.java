3cbd6375b1b2a5fff8f69ca1a17ac0a5928d73c0
Tim Brandt <timbrandtlopez@gmail.com>
2025-05-01 13:13:12 +0200

    Programación - 12/03/2025

---
+++ PROGRAMACIÓN/sesiones/sesion_2025_03_12/ConversionImplicita.java
@@ -0,0 +1,20 @@
+package sesiones.sesion_2025_03_12;
+
+public class ConversionImplicita {
+
+    public static void main(String[] args) {
+
+        // Declaramos una variable entera
+        int numeroEntero = 25;
+
+        // Conversión implícita de int a double
+        // Java permite esto automáticamente porque double tiene más capacidad que int
+        double numeroDecimal = numeroEntero;
+
+        // Imprimimos los valores antes y después de la conversión
+        System.out.println("Número entero: " + numeroEntero);
+        System.out.println("Número convertido a double: " + numeroDecimal);
+
+    }
+
+}
---
+++ PROGRAMACIÓN/sesiones/sesion_2025_03_12/IntToString.java
@@ -0,0 +1,17 @@
+package sesiones.sesion_2025_03_12;
+
+public class IntToString {
+
+    public static void main(String[] args) {
+
+        int numero = 456;
+
+        // Convertimos el número a String
+        String texto = String.valueOf(numero);
+
+        System.out.println("Número: " + numero);
+        System.out.println("Texto convertido: " + texto);
+
+    }
+
+}
