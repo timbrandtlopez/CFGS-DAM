6eea127acaec0e8e6e775c86d847de54acb0bc76
Tim Brandt <timbrandtlopez@gmail.com>
2025-05-01 13:12:30 +0200

    Programación - 05/03/2025

---
+++ PROGRAMACIÓN/sesiones/sesion_2025_03_05/OperadoresLogicos.java
@@ -0,0 +1,15 @@
+package sesiones.sesion_2025_03_05;
+
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+
+        boolean a = true, b = false;
+
+        System.out.println("a AND b: " + (a && b));
+        System.out.println("a OR b: " + (a || b));
+        System.out.println("NOT a: " + (!a));
+
+    }
+
+}
---
+++ PROGRAMACIÓN/sesiones/sesion_2025_03_05/OperadoresRelacionales.java
@@ -0,0 +1,16 @@
+package sesiones.sesion_2025_03_05;
+
+public class OperadoresRelacionales {
+
+    public static void main(String[] args) {
+
+        int x = 10, y = 20;
+
+        System.out.println("¿x es igual a y? " + (x == y));
+        System.out.println("¿x es diferente de y? " + (x != y));
+        System.out.println("¿x es mayor que y? " + (x > y));
+        System.out.println("¿x es menor que y? " + (x < y));
+
+    }
+
+}
